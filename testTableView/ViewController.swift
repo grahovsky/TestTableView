17977fd1c9cf4362db858420b76779767f4bbda9
Konstantin <grahovsky@gmail.com>
2018-10-03 10:04:25 +0300

    init

---
+++ testTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTableView
+//
+//  Created by Konstantin on 03/10/2018.
+//  Copyright © 2018 Konstantin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
